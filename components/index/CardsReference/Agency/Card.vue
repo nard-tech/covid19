59a43e68d756c58939eb7d8d7f2c1350ee2d3c17
Shu Fujita <s.fujita@nard.tech>
2021-06-28 03:48:57 +0900

    Fix annotation of components/index/CardsReference/Agency/Card.vue

--- components/index/CardsReference/Agency/Card.vue
+++ components/index/CardsReference/Agency/Card.vue
@@ -13,7 +13,12 @@
         :unit="$t('人')"
       >
         <template #additionalDescription>
-          {{ $t('※土・日・祝日を除く庁舎開庁日の1週間累計数') }}
+          <span>{{ $t('（注）') }}</span>
+          <ul>
+            <li>
+              {{ $t('土・日・祝日を除く庁舎開庁日の1週間累計数') }}
+            </li>
+          </ul>
         </template>
       </chart>
     </client-only>
